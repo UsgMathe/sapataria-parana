e674dd905bc47c1855f6f000463c3df07c89fa80
Matheus Gabriel Valenga <theusgvalenga@outlook.com>
2023-06-22 23:04:46 -0300

    new photos

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,83 @@
+"use client"
+
 import MainPage from "@/components/MainPage"
+import { useEffect, useState } from "react";
 
 export default function Home() {
+
+  const [randomPhoto, setRandomPhoto] = useState()
+  const [bgPhoto, setBgPhoto] = useState(true)
+
+  let photoList = []
+
+  useEffect(() => {
+    const loadImagesAsync = async () => {
+      const getImageInfoAsync = (image) => {
+        return new Promise((resolve, reject) => {
+          const img = new Image();
+          img.src = image.src;
+
+          img.onload = () => {
+            resolve({ src: image.src, width: img.width, height: img.height });
+          };
+
+          img.onerror = () => {
+            reject(new Error(`Failed to load image: ${image.src}`));
+          };
+        });
+      };
+      console.log("Loading images...");
+      const images = require.context('../../public/images/fotos', true);
+      const imagesList = images.keys().map(image => images(image).default);
+
+      const loadedPhotoList = await Promise.all(
+        imagesList.map(async (image) => {
+          const { src, width, height } = await getImageInfoAsync(image);
+          return { src, width, height, href: src };
+        })
+      );
+
+      photoList = loadedPhotoList;
+    };
+
+    loadImagesAsync();
+  }, [])
+
+  const randomizePhoto = () => {
+
+    if (photoList.length > 0) {
+      setRandomPhoto(photoList[Math.floor(Math.random() * photoList.length) - 1]);
+    }
+  }
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      randomizePhoto()
+    }, 6000)
+    return () => clearInterval(interval);
+  }, []);
+
   return (
     <MainPage>
-      <div className="absolute -z-10 blur-sm w-screen h-44">
-        <img src="/images/sapataria-frente.jpeg" alt="frente da sapataria paraná" className="object-cover w-full h-full animate-[pulse_6s_infinite]" />
+      <div className="absolute -z-10 blur-sm w-screen h-44" onClick={() => { console.log("SAJDLKAS") }}>
+        {
+          photoList && randomPhoto &&
+          <img key={randomPhoto.src} src={randomPhoto.src}
+            onLoadStart={() => {
+              setBgPhoto(true)
+            }}
+            onLoad={() => {
+              setBgPhoto(false)
+            }}
+            className={`object-cover w-full h-full animate-[pulse_6s_infinite] transition-all duration-700`}
+          />
+        }
+      </div>
+      <div className="absolute -z-20 blur-sm w-screen h-44">
+        {
+          bgPhoto &&
+          <img src={"/images/fotos/sapataria-frente.jpeg"} alt="" className="object-cover w-full h-full animate-[pulse_6s_infinite]" />
+        }
       </div>
       <header className="flex flex-col items-center mt-12">
         <h1 className=" text-4xl mb-2 font-semibold">Sapataria Paraná</h1>
